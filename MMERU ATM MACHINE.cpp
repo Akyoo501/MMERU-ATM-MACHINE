b39d8e5f69dec94ad81faf993a734f3a74638e66
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2025-01-02 19:23:10 +0300

    Update MMERU ATM MACHINE.cpp

--- MMERU ATM MACHINE.cpp
+++ MMERU ATM MACHINE.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
     int p;
     int pin = 1243;
-    int diposit = 0.0;
-    int balance = 0.0;
-    int withdraw = 0.0;
+    double diposit = 0.0;
+    double balance = 0.0;
+    double withdraw = 0.0;
     int choice;
     int count = 0;
     bool isTrue = true;
